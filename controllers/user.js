e723993d962dd18d3072e30d162d5c536a47aba2
ankit.kumar1 <ankit.kumar1@scotts.com>
2021-06-13 16:33:36 +0530

    changes done

--- controllers/user.js
+++ controllers/user.js
@@ -97,7 +97,8 @@ exports.logout = (req, res) => {
   req.session.destroy((err) => {
     if (err) console.log('Error : Failed to destroy the session during logout.', err);
     req.user = null;
-    res.redirect('/');
+    res.status(200).send({ msg: 'You have been logout'})
+
   });
 };
 
