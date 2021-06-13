3291476636eb4a863eefad137b59ed087ecf87a9
ankit.kumar1 <ankit.kumar1@scotts.com>
2021-06-13 22:35:15 +0530

    changes done

--- controllers/user.js
+++ controllers/user.js
@@ -542,7 +542,7 @@ exports.postForgot = (req, res, next) => {
     var mailOptions = {
     to: user.email,
     from: 'anki.kr7693@gmail.com',
-    subject: 'Reset your password on Hackathon Starter',
+    subject: '(IMPORTANT) Reset your password for gopalganj police login portal.',
     text: `You are receiving this email because you (or someone else) have requested the reset of the password for your account.\n\n
       Please click on the following link, or paste this into your browser to complete the process:\n\n
       http://${req.headers.host}/reset/${token}\n\n
